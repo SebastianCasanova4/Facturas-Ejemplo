9b0251ed87e59fa71023b2cbd0960fe396187497
SebastianCasanova4 <s.casanovao@uniandes.edu.co>
2024-06-26 22:16:01 -0500

    archivos

---
+++ mainScript.js
@@ -0,0 +1,33 @@
+function onOpen() {
+  showSidebar();  // Mostrar la barra lateral automáticamente al abrir el documento
+  var ui = SpreadsheetApp.getUi();
+  ui.createMenu('Custom Menu')
+    .addItem('Show sidebar', 'showSidebar')
+    .addToUi();
+}
+
+function showSidebar() {
+  var html = HtmlService.createHtmlOutputFromFile('Page')
+      .setTitle('Menú prueba');
+  SpreadsheetApp.getUi()
+      .showSidebar(html);
+}
+
+function openFacturaSheet() {
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var sheet = ss.getSheetByName("PreFactura");
+  SpreadsheetApp.setActiveSheet(sheet);
+}
+
+function openClientesSheet() {
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var sheet = ss.getSheetByName("Clientes");
+  SpreadsheetApp.setActiveSheet(sheet);
+}
+
+function openProductosSheet() {
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var sheet = ss.getSheetByName("Productos");
+  SpreadsheetApp.setActiveSheet(sheet);ç
+  
+}
